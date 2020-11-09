53ec000c3be8486c13dde73110073c34b9294154
priyasharma2692 <68745409+priyasharma2692@users.noreply.github.com>
2020-11-09 16:43:20 +0530

    Add files via upload

--- Assignment 3/pangram.py
+++ Assignment 3/pangram.py
@@ -1,1 +1,12 @@
+import string
 
+inputString = input("Enter the string : ")
+allAlphabets = 'abcdefghijklmnopqrstuvwxyz'
+flag = 0
+for char in allAlphabets:
+    if char not in inputString.lower():
+        flag = 1
+if flag == 1:
+    print("This is not a pangram string");
+else:
+    print("It is a pangram string")
