abf66c67f9cebd86872d1b463de8a850ab2b3154
priyasharma2692 <68745409+priyasharma2692@users.noreply.github.com>
2020-11-09 18:34:51 +0530

    Add files via upload

---
+++ Assignment 3/cyber 3_6.py
@@ -0,0 +1,17 @@
+def blackjack(a,b,c):
+    
+    if sum((a,b,c)) <= 21:
+        return sum((a,b,c))
+    elif sum((a,b,c)) <=31 and 11 in (a,b,c):
+        return sum((a,b,c)) - 10
+    else:
+        return 'BUST'
+
+# Check
+blackjack(5,6,7)
+
+# Check
+blackjack(9,9,9)
+
+# Check
+blackjack(9,9,11)
